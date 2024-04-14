e72e8fee82757819d2f57799885aa4291ff9faec
Marcelo Yugo <marcelo.tonossu@engineeringhub.man>
2024-04-14 01:40:41 +0100

    add .env file

--- src/services/event.js
+++ src/services/event.js
@@ -1,16 +1,10 @@
 import axios from "axios";
 
 
-const getAuthConfig = () => ({
-    headers: {
-        Authorization: `Bearer ${localStorage.getItem("access_token")}`
-    }
-})
 export const getEvents = async () => {
     try {
         return await axios.get(
-            `${import.meta.env.VITE_API_BASE_URL}/api/v1/events`,
-            getAuthConfig()
+            `${import.meta.env.VITE_API_BASE_URL}/events`
         )
     } catch (e) {
         throw e;
